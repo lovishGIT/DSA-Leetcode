ee3a36a3608c745379d13f971d0e63cc12f61115
Lovish Bansal <127772046+lovishGIT@users.noreply.github.com>
2025-01-05 07:11:31 +0530

    Time: 107 ms (7.96%), Space: 55.4 MB (27.5%) - LeetHub

---
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -0,0 +1,50 @@
+class Solution {
+    private int merge(int[] arr, int left, int pivot, int right) {
+        int count = 0;
+        // counting
+        int st = left;
+        int mid = pivot;
+        while(st < pivot) {
+            long prod = 0;
+            if(mid <= right) prod = 2 * (long)(arr[mid]);
+            if(mid <= right && arr[st] > prod) {
+                mid++;
+            } else {
+                count += mid - pivot;
+                st++;
+            }
+        }
+        // sort into diff array 
+        st = left;
+        mid = pivot;
+        ArrayList<Integer> store = new ArrayList<>();
+        while(st < pivot && mid <= right) {
+            if(arr[st] <= arr[mid]) {
+                store.add(arr[st++]);
+            } else {
+                store.add(arr[mid++]);
+            }
+        }
+        while(st < pivot) store.add(arr[st++]);
+        while(mid <= right) store.add(arr[mid++]);
+
+        // assignning
+        st = left;
+        for(int i: store) {
+            arr[st++] = i;
+        }
+        return count;
+    }
+    private int doSort(int[] nums, int st, int ed) {
+        if(st < 0 || ed >= nums.length || st==ed) return 0;
+        int mid = (st + ed) / 2;
+        int count = 0;
+        count += doSort(nums, st, mid);
+        count += doSort(nums, mid+1, ed);
+        count += merge(nums, st, mid+1, ed);
+        return count;
+    }
+    public int reversePairs(int[] nums) {
+        return doSort(nums, 0, nums.length - 1);
+    }
+}
