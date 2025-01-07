8ec88701b3ae245d561e7323197a6a61c8da87ee
Lovish Bansal <127772046+lovishGIT@users.noreply.github.com>
2025-01-07 23:49:01 +0530

    Time: 0 ms (100%), Space: 41.2 MB (84.65%) - LeetHub

---
+++ 0521-longest-uncommon-subsequence-i/0521-longest-uncommon-subsequence-i.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findLUSlength(String a, String b) {
+        if(a.length() < b.length()) return b.length();
+        if(a.length() > b.length()) return a.length();
+
+        for(int i= 0; i<a.length(); i++) {
+            if(a.charAt(i) != b.charAt(i)) return a.length();
+        }
+
+        return -1;
+
+    }
+}
