e4aaeecb880bef42f197b8a97a5d08e2ff0a6982
Lovish Bansal <127772046+lovishGIT@users.noreply.github.com>
2025-01-07 23:54:00 +0530

    Time: 59 ms (10.4%), Space: 49.1 MB (8.59%) - LeetHub

---
+++ 2809-create-hello-world-function/2809-create-hello-world-function.js
@@ -0,0 +1,10 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        // console.log("Hello World")
+        return "Hello World"
+    }
+};
