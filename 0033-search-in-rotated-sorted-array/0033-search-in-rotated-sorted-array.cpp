465231a2a9b9443f55ca7264bd40e6d91ae997b8
Lovish Bansal <127772046+lovishGIT@users.noreply.github.com>
2025-01-25 01:03:53 +0530

    Time: 0 ms (100%), Space: 15.4 MB (32.24%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -5,15 +5,11 @@ public:
         int last  = nums.size() - 1;
 
         while(first <= last) {
-            // cout << first << " " << last << endl;
             int mid = first + (last - first) / 2;
 
             if(nums[mid] == target) return mid;
 
-            if(nums[first] <= nums[mid] && nums[mid] <= nums[last]) {
-                if(nums[mid] < target) first = mid + 1;
-                else last = mid - 1;
-            } else if(nums[first] <= nums[mid]) {
+            if(nums[first] <= nums[mid]) {
                 if(nums[first] <= target && target < nums[mid]) last = mid - 1;
                 else first = mid + 1;
             } else {
@@ -21,7 +17,6 @@ public:
                 else last = mid - 1;
             }
         }
-        // cout << first << " " << last << endl;
         return -1;
     }
 };
