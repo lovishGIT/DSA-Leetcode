10d3049b3f5aabbc149e86cc3c7f219876573f8c
Lovish Bansal <127772046+lovishGIT@users.noreply.github.com>
2025-01-26 23:18:10 +0530

    Time: 0 ms (100%), Space: 8.7 MB (15.18%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -5,19 +5,16 @@ public:
         int first = 1;
         int last = x - 1;
 
-        int ans = 1;
-
         while(first <= last) {
             int mid = first + (last - first) / 2;
 
             if(mid <= x / mid) {
-                ans = max(ans, mid);
                 first = mid + 1;
             } else {
                 last = mid - 1;
             }
         }
 
-        return ans;
+        return last;
     }
 };
