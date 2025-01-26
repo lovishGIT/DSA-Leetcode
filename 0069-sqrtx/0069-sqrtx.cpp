fe2f028ae8ca740ca1b27b3427259162e9986c90
Lovish Bansal <127772046+lovishGIT@users.noreply.github.com>
2025-01-26 23:17:03 +0530

    Time: 0 ms (100%), Space: 8.6 MB (15.18%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,11 +1,23 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if(x<=1) return x;
-        long i=1; 
-        while(x >= (i*i)){
-            i++;
+        if(x < 2) return x;
+        int first = 1;
+        int last = x - 1;
+
+        int ans = 1;
+
+        while(first <= last) {
+            int mid = first + (last - first) / 2;
+
+            if(mid <= x / mid) {
+                ans = max(ans, mid);
+                first = mid + 1;
+            } else {
+                last = mid - 1;
+            }
         }
-        return (i-1);
+
+        return ans;
     }
 };
