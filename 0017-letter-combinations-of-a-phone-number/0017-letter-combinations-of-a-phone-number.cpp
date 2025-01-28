8894396ffab759e2e70ab53a45a0399e02efe895
Lovish Bansal <127772046+lovishGIT@users.noreply.github.com>
2025-01-28 05:35:24 +0530

    Time: 0 ms (100%), Space: 9 MB (95.31%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -15,22 +15,16 @@ public:
     }
     vector<string> configure(vector<string> temp, string chars) {
         vector<string> answers;
-        if(temp.size() == 0) {
+        for(auto i: temp) {
             for(auto j: chars) {
-                string s(1, j);
-                answers.push_back(s);
-            }
-        } else {
-            for(auto i: temp) {
-                for(auto j: chars) {
-                    answers.push_back(i + "" + j);
-                }
+                answers.push_back(i + "" + j);
             }
         }
         return answers;
     }
     vector<string> letterCombinations(string digits) {
-        vector<string> answers;
+        if(digits.length() <= 0) return {};
+        vector<string> answers = {""};
         for(auto i: digits) {
             answers = configure(answers, get_alphabets(i));
         }
