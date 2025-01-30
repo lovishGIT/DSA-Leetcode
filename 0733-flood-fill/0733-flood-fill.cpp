257469750263f16a4397182b35c9afcf9dc1df50
Lovish Bansal <127772046+lovishGIT@users.noreply.github.com>
2025-01-30 10:35:32 +0530

    Time: 1 ms (22.9%), Space: 18.2 MB (59.72%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int oldColor = image[sr][sc];
+        image[sr][sc] = color;
+
+        if(oldColor == color) return image;
+
+        queue<pair<int, int>> que;
+        que.push({sr, sc});
+
+        int changeRow[] = {-1, 0, 1,  0};
+        int changeCol[] = { 0, 1, 0, -1};
+
+        while(!que.empty()) {
+            int currR = que.front().first;
+            int currC = que.front().second;
+            que.pop();
+
+            for(int i =0; i < 4; i++) {
+                int nr = currR + changeRow[i];
+                int nc = currC + changeCol[i];
+
+                if(nr >= 0 && nc >= 0 && nr < image.size() && nc < image[0].size() && image[nr][nc] == oldColor) {
+                    image[nr][nc] = color;
+                    que.push({nr, nc});
+                }
+            }
+        }
+
+        return image;
+    }
+};
