3dcdc5e987ab9124467faf83077d13209491b6b1
Lovish Bansal <127772046+lovishGIT@users.noreply.github.com>
2025-02-06 21:04:47 +0530

    Time: 14 ms (5.27%), Space: 16.2 MB (5.08%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> func(vector<int> nums, vector<int> tillNow, vector<int> vis) {
+    vector<vector<int>> func(vector<int> nums, vector<int> tillNow, int start) {
         if(tillNow.size() == nums.size()) return {tillNow};
 
         vector<vector<int>> answers;
         vector<int> temp;
         for (auto ele: tillNow) temp.push_back(ele);
 
-        for(int i = 0; i < nums.size(); i++) {
-            if(vis[i] == 1) continue;
-
+        for(int i = start; i < nums.size(); i++) {
             vector<int> temp1 = temp;
-            
             temp1.push_back(nums[i]);
-            vis[i] = 1;
-            for(auto ele: func(nums, temp1, vis)) {
+            swap(nums[i], nums[start]);
+            for(auto ele: func(nums, temp1, start + 1)) {
                 answers.push_back(ele);
             }
-            vis[i] = 0;
+            swap(nums[i], nums[start]);
         }
         return answers;
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> answers;
-        vector<int> vis(nums.size(), 0);
-        answers = func(nums, {}, vis);
+        answers = func(nums, {}, 0);
         return answers;
     }
 };
