2b131c06013b560811935f90e894b9b9e78b5a6b
Lovish Bansal <127772046+lovishGIT@users.noreply.github.com>
2025-02-08 10:07:59 +0530

    Time: 0 ms (100%), Space: 13.4 MB (27.77%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
@@ -3,7 +3,6 @@ public:
     int findKthPositive(vector<int>& arr, int k) {
         int left = 0;
         int right = arr.size() - 1;
-        int answer = 0;
 
         while(left <= right) {
             int mid = left + (right - left) / 2;
