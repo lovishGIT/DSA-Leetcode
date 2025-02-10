5324ebdd397a6b05b47a5d8a38192aa596f1a109
Lovish Bansal <127772046+lovishGIT@users.noreply.github.com>
2025-02-10 05:53:30 +0530

    Time: 3 ms (33.3%), Space: 95.1 MB (80.92%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -3,34 +3,43 @@ public:
     double findMedianSortedArrays(vector<int>& A, vector<int>& B) {
         int n= A.size();
         int m= B.size();
-        int ind = 0; 
-        int x = 0;
 
-        vector<int> ans;
-        // merge(A.begin(),A.end(), B.begin(), B.end(), ans.begin());
-        // sort(ans.begin(), ans.end());
+        int ind2 = (n+m) / 2;
+        int ind1 = ind2 - 1;
 
-        while(ind<n || x<m) {
-            // cout<< ind << " " << x << endl;
-            if(ind<n && (x>=m || A[ind]<B[x])) {
-                ans.push_back(A[ind]);
-                ind++;
-            } else {
-                ans.push_back(B[x]);
-                x++;
-            }
-        }
-
-        // for(auto i: ans) {
-        //     cout<< i << " ";
-        // } cout<< endl;
-
-        int mid = ans.size() / 2;
-        if(ans.size() % 2==0) {
-            return (ans[mid] + ans[mid-1]) / 2.0;
-        } else {
-            return ans[mid];
-        }
+        int ele1 = INT_MIN;
+        int ele2 = INT_MIN;
         
+        int cnt = 0;
+        int i = 0, j = 0;
+        while(cnt < n+m) {
+            int ele = -1;
+            if(i < n && j < m) {
+                if(A[i] <= B[j]) {
+                    ele = A[i];
+                    i++;
+                } else {
+                    ele = B[j];
+                    j++;
+                }
+            } else if(i < n) {
+                ele = A[i];
+                i++;
+            } else {
+                ele = B[j];
+                j++;
+            }
+
+            if(cnt == ind1) ele1 = ele;
+            else if(cnt == ind2) ele2 = ele;
+            cnt++;
+        }
+
+        // cout << ind1 << " -> " << ele1 << " && " << ind2 << " -> " << ele2 << endl;
+        if((n+m) %2 == 0) {
+            return (ele1+ele2) / 2.0;
+        } else {
+            return ele2;
+        }
     }
 };
