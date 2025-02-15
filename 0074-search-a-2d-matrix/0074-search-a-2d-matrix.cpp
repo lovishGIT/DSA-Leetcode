10d39b6bac3b7d7f5595e963d3010f5687937ab0
Lovish Bansal <127772046+lovishGIT@users.noreply.github.com>
2025-02-15 23:21:55 +0530

    Time: 0 ms (100%), Space: 13.4 MB (7.62%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -3,28 +3,18 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int nr = matrix.size();
         int nc = matrix[0].size();
-        int n = nr * nc;
 
         int first = 0;
-        int last = n;
+        int last = nr * nc;
         while(first <= last) {
             int mid = first + (last - first) / 2;
-            cout << mid/nc << " " << mid%nc << endl;
-            if(mid/nc >= nr) {
+            if(mid/nc >= nr || mid%nc >= nc) {
                 last = mid - 1;
                 continue;
             }
-            if(mid%nc >= nc) {
-                last = mid - 1;
-                continue;
-            }
-            if(matrix[mid/nc][mid%nc] == target) {
-                return true;
-            } else if(matrix[mid/nc][mid%nc] > target) {
-                last = mid - 1;
-            } else {
-                first = mid + 1;
-            }
+            if(matrix[mid/nc][mid%nc] == target) return true;
+            else if(matrix[mid/nc][mid%nc] > target) last = mid - 1;
+            else first = mid + 1;
         }
 
         return false;
