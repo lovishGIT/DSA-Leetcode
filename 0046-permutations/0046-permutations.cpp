a552b1286fcdd9a051fe0c9222e86cd19153c7ec
Lovish Bansal <127772046+lovishGIT@users.noreply.github.com>
2025-03-09 17:12:33 +0530

    Time: 14 ms (5.51%), Space: 16.2 MB (5.36%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> func(vector<int> nums, vector<int> tillNow, int start) {
-        if(tillNow.size() == nums.size()) return {tillNow};
-
+    vector<vector<int>> func(vector<int> nums, vector<int> tillNow, int ind) {
+        if(ind >= nums.size()) return {tillNow};
         vector<vector<int>> answers;
-        vector<int> temp;
-        for (auto ele: tillNow) temp.push_back(ele);
-
-        for(int i = start; i < nums.size(); i++) {
-            vector<int> temp1 = temp;
-            temp1.push_back(nums[i]);
-            swap(nums[i], nums[start]);
-            for(auto ele: func(nums, temp1, start + 1)) {
-                answers.push_back(ele);
-            }
-            swap(nums[i], nums[start]);
+        for(int i = ind; i < nums.size(); i++) {
+            vector<int> temp = tillNow;
+            temp.push_back(nums[i]);
+            // swap first
+            int var1 = nums[ind];
+            nums[ind] = nums[i];
+            nums[i] = var1;
+            // recursion
+            for(auto i: func(nums, temp, ind+1)) answers.push_back(i);
+            // swap back
+            int var2 = nums[ind];
+            nums[ind] = nums[i];
+            nums[i] = var2;
         }
         return answers;
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> answers;
-        answers = func(nums, {}, 0);
+        for(auto i: func(nums, {}, 0)) answers.push_back(i);
         return answers;
     }
 };
