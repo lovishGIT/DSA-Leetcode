9c9a905b3a089ea06c1c4db8756f6f4c110c7188
Lovish Bansal <127772046+lovishGIT@users.noreply.github.com>
2025-05-28 10:20:54 +0530

    Time: 0 ms (100%), Space: 19.4 MB (86.46%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -1,35 +1,32 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode* ans = new ListNode(-1);
-        ListNode* ansStore = ans;
-        while(l1!= nullptr && l2 != nullptr) {
+        ListNode* newlist = new ListNode();
+        ListNode* temp = newlist;
+
+        while(l1!=nullptr && l2!=nullptr) {
             if(l1->val <= l2->val) {
-                ans->next = l1;
-                l1 = l1->next;
+                newlist->next = l1;
+                l1 = l1->next; 
             } else {
-                ans->next = l2;
-                l2 = l2->next;
+                newlist->next = l2;
+                l2 = l2->next; 
             }
-            ans = ans->next;
-        }
-        if(l1!= nullptr) {
-            ans->next = l1;
-        }
-        if(l2 != nullptr) {
-            ans->next = l2;
+            newlist = newlist->next;
         }
 
-        return ansStore->next;
+        while(l1!=nullptr) {
+            newlist->next = l1;
+            l1 = l1->next;
+            newlist = newlist->next;
+        }
+
+        while(l2!=nullptr) {
+            newlist->next = l2;
+            l2 = l2->next;  
+            newlist = newlist->next;
+        }
+
+        return temp->next;
     }
 };
